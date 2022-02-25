0156aa25c23269ec43805a19a198da336ef98610
tokamostafa <totaelbery@gmail.com>
2022-02-25 17:16:33 +0200

    selected

--- components/screens/selected pet.js
+++ components/screens/selected pet.js
@@ -2,12 +2,6 @@ import * as React from 'react';
 import { Text, View, StyleSheet,SafeAreaView,StatusBar,ScrollView,TouchableOpacity } from 'react-native';
 
 export const SelectedPet=()=>{
-  /*function Adopt(){
-   navigation.navigate('AdoptionList')
-  }
-  function Breed(){
-    navigation.navigate('BreedingList')
-   }*/
   return(
   <View style={styles.container1}>
      <Text>Welcome to selected pet</Text>
